36520f5b384cbe0f70cecec3839972b8de88d9fa
cholo5ntrol <raglaks94@gmail.com>
2018-09-15 10:46:16 -0500

    initial firebase code

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA7zNm3UlArhH5H-jZTlHH9KJlprKBtebU",
+    authDomain: "timesheet-2881c.firebaseapp.com",
+    databaseURL: "https://timesheet-2881c.firebaseio.com",
+    projectId: "timesheet-2881c",
+    storageBucket: "timesheet-2881c.appspot.com",
+    messagingSenderId: "89991514141"
+  };
+  firebase.initializeApp(config);
+
+  $("")
